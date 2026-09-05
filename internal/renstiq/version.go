2a48acc05d168f238e0262d16b91dc0f28e346b6
wim <45655999+wim-web@users.noreply.github.com>
2026-09-06 07:45:23 +0900

    feat: release-pleaseによるリリースとmatrixビルドを追加 (#3)

--- internal/renstiq/version.go
+++ internal/renstiq/version.go
@@ -2,15 +2,20 @@ package renstiq
 
 import "runtime/debug"
 
-// Version can be set with -ldflags when building release binaries.
+// Version and Commit can be set with -ldflags when building release binaries.
 var Version string
+var Commit string
 
 func buildVersion() string {
-	if Version != "" {
-		return Version
+	version := Version
+	if version == "" {
+		version = "dev"
+		if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "" && info.Main.Version != "(devel)" {
+			version = info.Main.Version
+		}
 	}
-	if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "" && info.Main.Version != "(devel)" {
-		return info.Main.Version
+	if Commit != "" {
+		return version + " (" + Commit + ")"
 	}
-	return "dev"
+	return version
 }
